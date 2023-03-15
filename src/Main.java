4001a1ac652225067bfc6182226e6955b03b6bb9
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-03-15 21:41:30 +0300

    Вычислить факториал целых чисел от 1 до 10 с помощью цикла while.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+/*
+Вычислить факториал целых чисел от 1 до 10 с помощью цикла while.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Вычисление факториала");
+        final int FACTOR = 10;
+        int i = 1;
+        int result = 1;
+        while (i <= FACTOR) {
+            result *= i;
+            System.out.printf("Факториал числа %d равен %d\n", i, result);
+            i++;
+        }
     }
 }
